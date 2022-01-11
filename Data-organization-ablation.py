5b915ca0cb536e304855856f9baf9f7ff6065da4
Jing Yang <jing.yang@dl-23.recod>
2022-01-11 10:02:53 -0300

    add code

--- Data-organization-ablation.py
+++ Data-organization-ablation.py
@@ -99,77 +99,45 @@ grouped_sort = df_input.groupby('docid')
 
 df_aggre_again = grouped_sort.aggregate(lambda x: tuple(x))
 
-NumofQ = 10
-docid = []
-qid = []
-for index,row in df_aggre_again.iterrows():
-    L = len(row['qid'])
-    if L >= NumofQ:
-        for j in range(NumofQ):
-            docid.append(index)
-            qid.append(row['qid'][j])
-    else:
-        for j in range(L):
-            docid.append(index)
-            qid.append(row['qid'][j])
-        for j in range(L, NumofQ):
-            docid.append(index)
-            qid.append(row['qid'][0])
-            
-dict_organize = {
-
-    'docid':docid,
-    'qid':qid
-}
-
-df_organize = pd.DataFrame(dict_organize, columns = ['docid','qid'])
-df_all = pd.merge(df_organize, df_input, on=['docid', 'qid'], how='left')
-grouped_org = df_all.groupby('docid')
-df_aggre_10 = grouped_org.aggregate(lambda x: tuple(x))
 cid = []
 docid = []
 claim = []
 gold_evidence = []
-qas = []
+q_concat = []
+ac_concat = []
+ag_concat = []
 label = []
-
-for j in range(10):
-    qas.append([])
-    
-for index,row in df_aggre_10.iterrows():
+for index,row in df_aggre_again.iterrows():
     docid.append(index)
     cid.append(row['id'][0])
     claim.append(row['text'][0])
     gold_evidence.append(row['gold_evi_concat'][0])
-    
-    for j in range(10):
-        qas[j].append([row['question'][j], row['answer_text'][j],  row['answer_gold'][j]])
-
+    q_concat.append(' '.join(row['question']))
+    ac_concat.append('\n'.join(row['answer_text']))
+    ag_concat.append('\n'.join(row['answer_gold']))
     label.append(row['label'][0])
-
-    
             
-dict_prepare = {
+dict_final = {
 
     'docid':docid,
     'id':cid,
     'text':claim,
     'gold_evi_concat':gold_evidence,
-    'qa1':qas[0], 'qa2':qas[1], 'qa3':qas[2], 'qa4':qas[3], 'qa5':qas[4],
-    'qa6':qas[5], 'qa7':qas[6], 'qa8':qas[7], 'qa9':qas[8], 'qa10':qas[9],
+    'question':q_concat,
+    'answer_text':ac_concat,
+    'answer_gold':ag_concat,
     'label':label,
 
 }
 
-df_input_nocat = pd.DataFrame(dict_prepare, columns = ['docid','id','text', 'gold_evi_concat',
-                                                'qa1', 'qa2', 'qa3', 'qa4', 'qa5', 
-                                                'qa6', 'qa7', 'qa8', 'qa9', 'qa10', 
-                                                'label'])
+df_input_cat = pd.DataFrame(dict_final, columns = ['docid','id','text','question', 'gold_evi_concat',
+                                                     'answer_text','answer_gold',
+                                                     'label'])
 
-df_train_set = df_input_nocat[df_input_nocat['id'].isin(set(df_train['id']))]
-df_dev_set = df_input_nocat[df_input_nocat['id'].isin(set(df_dev['id']))]
-df_test_set = df_input_nocat[df_input_nocat['id'].isin(set(df_test['id']))]
+df_train_set = df_input_cat[df_input_cat['id'].isin(set(df_train['id']))]
+df_dev_set = df_input_cat[df_input_cat['id'].isin(set(df_dev['id']))]
+df_test_set = df_input_cat[df_input_cat['id'].isin(set(df_test['id']))]
 
-df_train_set.to_pickle('qa-fool-me-twice-train-nocat-cqaa-electra.pkl')
-df_dev_set.to_pickle('qa-fool-me-twice-dev-nocat-cqaa-electra.pkl')
-df_test_set.to_pickle('qa-fool-me-twice-test-nocat-cqaa-electra.pkl')
+df_train_set.to_pickle('qa-fool-me-twice-train-oria-electra.pkl')
+df_dev_set.to_pickle('qa-fool-me-twice-dev-oria-electra.pkl')
+df_test_set.to_pickle('qa-fool-me-twice-test-oria-electra.pkl')
